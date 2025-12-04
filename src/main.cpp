3f5909883c84508161a41c8711104c4dc642c721
rrv2004 <rohanaathira22@gmail.com>
2025-12-04 22:56:14 +0530

    type base implementation

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,20 @@ int main() {
     std::getline(std::cin, command);
     if (command == "exit") {
       break;
-    } else if (command.substr(0,4) == "echo" ) {
+    }
+    else if (command.substr(0,4) == "echo" ) {
       std::cout << command.substr(5) << std::endl;
-    } else {
+    }
+    else if(command.substr(0,4)=="type"){
+      std:: string arg=command.substr(5);
+      if(arg=="exit"||arg=="echo"){
+        std::cout<<command<<"is a shell builtin"<<std::endl;
+      }
+      else{
+        std::cout<<command<<": not found"<<std::endl;
+      }
+    }
+    else {
     std::cout << command << ": command not found" << std::endl;
     }
   }
