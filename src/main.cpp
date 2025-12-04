d84d9539d5210fdc635a00926ce3fd1c092c0065
rrv2004 <rohanaathira22@gmail.com>
2025-12-04 22:43:46 +0530

    echo base implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,22 @@
 #include <iostream>
 #include <string>
 
-void echo(std::string arg){
-  std::cout<<arg<<std::endl;
-}
-void repl(){
-  while(true){
+int main() {
+  while (true) {
+
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+
     std::cout << "$ ";
     std::string command;
-    std::getline(std::cin,command);
-    if(command=="exit"){
+    std::getline(std::cin, command);
+    if (command == "exit") {
       break;
+    } else if (command.substr(0,4) == "echo" ) {
+      std::cout << command.substr(5) << std::endl;
+    } else {
+    std::cout << command << ": command not found" << std::endl;
     }
-    if(command.substr(0,4)=="echo"){
-      int l=4;
-      while(command[l]==' '){
-        l++;
-      }
-      echo(command.substr(l));
-    }
-    std::cout<<command<<": command not found"<<std::endl;
   }
-}
-int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
-  repl();
-  
 }
