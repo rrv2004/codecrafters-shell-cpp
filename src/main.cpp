d119a85a7114e2c4637cb8ad1fe898e112a69ac7
rrv2004 <rohanaathira22@gmail.com>
2025-12-04 22:29:05 +0530

    exit base implementation

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,21 @@
 #include <string>
 
 void repl(){
-  std::cout << "$ ";
-  std::string command;
-  std::getline(std::cin,command);
-  std::cout<<command<<": command not found"<<std::endl;
+  while(true){
+    std::cout << "$ ";
+    std::string command;
+    std::getline(std::cin,command);
+    if(command=="exit"){
+      break;
+    }
+    std::cout<<command<<": command not found"<<std::endl;
+  }
 }
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  while(true){
-    repl();
-  }
+  repl();
+  
 }
